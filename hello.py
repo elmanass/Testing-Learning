61fa61260ca97a7867a357527d02e6d29f361c42
elmanass <90607383+elmanass@users.noreply.github.com>
2021-09-13 12:06:56 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+echo "# Testing-Learning" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/elmanass/Testing-Learning.git
+git push -u origin main
